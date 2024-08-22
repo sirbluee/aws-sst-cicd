f44b125995e5d201a5d35bbbab294cf1baf28e6d
sirblue <sophearum14@gmail.com>
2024-08-22 09:59:39 +0700

    modified pipeline

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function BlogPage() {
   return (
     <>
-      <h1>BlogPage</h1>
+      <div className="h-screen grid grid-cols place-content-center">
+        <h1>BlogPage</h1>
+      </div>
     </>
   );
 }
