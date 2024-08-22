2c3cda5c80a076d1da73a80beb53c869d2c7cf60
sirblue <sophearum14@gmail.com>
2024-08-22 09:19:13 +0700

    added page blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function BlogPage() {
   return (
-    <div>BlogPage</div>
-  )
+    <>
+      <h1>BlogPage</h1>
+    </>
+  );
 }
